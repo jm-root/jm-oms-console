a5bdd1f1b7b9bd274b85020dff1151078a41979c
fjy <531480056@qq.com>
2017-01-18 17:49:29 +0800

    登录

--- src/apps/main/js/controllers/signin.js
+++ src/apps/main/js/controllers/signin.js
@@ -1,23 +1,64 @@
 'use strict';
 
 /* Controllers */
-  // signin controller
-app.controller('SigninFormController', ['$scope', '$http', '$state', function($scope, $http, $state) {
+// signin controller
+app.controller('SigninCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+    var sso = jm.sdk.sso;
     $scope.user = {};
-    $scope.authError = null;
-    $scope.login = function() {
-      $scope.authError = null;
-      // Try to login
-      $http.post('api/login', {email: $scope.user.email, password: $scope.user.password})
-      .then(function(response) {
-        if ( !response.data.user ) {
-          $scope.authError = 'Email or Password not right';
-        }else{
-          $state.go('app.dashboard-v1');
-        }
-      }, function(x) {
-        $scope.authError = 'Server Error';
-      });
+    $scope.visImgCode = localStorage.getItem('visImgCode')=='true';
+
+    $scope.authError = sso.authError || null;
+    var isWXLongin = localStorage.getItem('isWXLogin');
+    $scope.isWXLongin = !!(isWXLongin=='1'||isWXLongin=='true');
+
+    $scope.login = function () {
+        localStorage.setItem('isWXLogin', false);
+        $scope.authError = null;
+        sso.signon($scope.user, function(err, result){
+            console.log(result);
+            if(result.token){
+                localStorage.setItem('token',result.token);
+                localStorage.setItem('id',result.id);
+                localStorage.setItem('visImgCode', false);
+                $scope.visImgCode = false;
+                $state.go('app');
+            }else{
+                $scope.authError = '账号或密码错误';
+                if(result.err===2006||result.err===2007){
+                    localStorage.setItem('visImgCode', true);
+                    $scope.visImgCode = true;
+                }
+                if($scope.visImgCode) $scope.refresh();
+                if(result.err===2007) $scope.authError = '验证码错误';
+                $scope.isWXLongin = false;
+            }
+        });
     };
-  }])
-;
+
+    $scope.weixin = function(){
+        $scope.authError =
+        localStorage.setItem('isWXLogin', true);
+        $http.get(ssoUri+'/wechat/authuri', {
+            params: {
+                process_uri:'http://'+$scope.host+'/wechat/oauth',
+                process_uri2:'http://'+$scope.host+'/wechat_oauth.html',
+                web: 1,
+                uri:'http://'+$scope.host+'/admin'
+            }
+        }).success(function(result){
+            window.location = result.uri;
+        }).error(function(msg, code){
+        });
+    };
+
+    $scope.onInput = function(){
+        if($scope.authError) $scope.authError = null;
+    };
+
+    $scope.refresh = function(){
+        var rkey = Date.now();
+        $scope.user.rkey = rkey;
+        $scope.imgCodeUri = uploadUri+'/imgCode/'+rkey+'/img.jpg';
+    };
+    $scope.refresh();
+}]);
