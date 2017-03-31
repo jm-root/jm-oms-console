a01fcf3ae147595907977afa374e0665a1e76a78
jeff <jeff@jamma.cn>
2017-04-01 00:39:26 +0800

    增加取消按键

--- src/apps/search/js/controllers/index.js
+++ src/apps/search/js/controllers/index.js
@@ -1,4 +1,9 @@
 'use strict';
 app.controller('SearchCtrl',['$scope','$translatePartialLoader',function ($scope,$translatePartialLoader) {
     $translatePartialLoader.addPart('common');
+
+    $scope.cancel = function () {
+        window.history.go( -1 );
+        window.location.reload();//刷新当前页面
+    };
 }]);
