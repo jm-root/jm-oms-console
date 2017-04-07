bf318241f75660273eac83cd38b2742fbf2e55d8
ayan <820695483@qq.com>
2017-04-07 12:27:34 +0800

    增加数据统计文件

---
+++ src/apps/datastatistics/js/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function(){
+    var name = 'datastatistics';
+    var path = 'apps/'+ name + '/';
+
+    /**
+     * Config for the router
+     */
+    angular.module('app')
+        .config(
+            [          '$stateProvider', '$urlRouterProvider', 'lazyLoadProvider',
+                function ($stateProvider,   $urlRouterProvider, lazyLoadProvider) {
+                    $stateProvider
+                        .state('app.datastatistics', {
+                            url: '/datastatistics',
+                            template: '<div ui-view class="fade-in-down"></div>',
+                            controller:'DataStatisticsCtrl',
+                            resolve: lazyLoadProvider.load( [path + 'js/controllers/index.js',path + 'js/controllers/agent.js',path + 'js/controllers/player.js'] )
+                        })
+                        .state('app.datastatistics.agentdata', {
+                            url: '/agentdata',
+                            templateUrl: path + 'tpl/agent.data.html',
+                            controller: 'AgentDataCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                        .state('app.datastatistics.agentstatistics', {
+                            url: '/agentstatistics',
+                            templateUrl: path + 'tpl/agent.statistics.html',
+                            controller: 'AgentStatisticsCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                        .state('app.datastatistics.agentdiary', {
+                            url: '/agentdiary',
+                            templateUrl: path + 'tpl/agent.diary.html',
+                            controller: 'AgentDiaryCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                        .state('app.datastatistics.playerdata', {
+                            url: '/playerdata',
+                            templateUrl: path + 'tpl/player.data.html',
+                            controller: 'PlayerDataCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                        .state('app.datastatistics.playerstatistics', {
+                            url: '/playerstatistics',
+                            templateUrl: path + 'tpl/player.statistics.html',
+                            controller: 'PlayerStatisticsCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                        .state('app.datastatistics.playerdiary', {
+                            url: '/playerdiary',
+                            templateUrl: path + 'tpl/player.diary.html',
+                            controller: 'PlayerDiaryCtrl',
+                            resolve: lazyLoadProvider.load('daterangepicker')
+                        })
+                }
+            ]
+        );
+}());
+
