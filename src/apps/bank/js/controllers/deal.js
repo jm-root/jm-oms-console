4401de410a6941b867c914561c9f359dce309e9d
Qukoko <15016630905@163.com>
2017-04-18 21:17:31 +0800

    修改银行交易流水页面

--- src/apps/bank/js/controllers/deal.js
+++ src/apps/bank/js/controllers/deal.js
@@ -3,7 +3,10 @@ app.controller('BankDealCtrl', ['$scope', '$state', '$http', 'global', function 
     var history = global.bankDealHistory||(global.bankDealHistory={});
     $scope.pageSize = history.pageSize||$scope.defaultRows;
     $scope.search = history.search||{};
-    $scope.search.date = $scope.search.date || {};
+    $scope.search.date = $scope.search.date || {
+            startDate:  moment().subtract(15, 'days'),
+            endDate: moment()
+        };
 
     $scope.dateOptions = angular.copy(global.dateRangeOptions);
     $scope.dateOptions.opens = 'left';
@@ -52,29 +55,45 @@ app.controller('BankDealCtrl', ['$scope', '$state', '$http', 'global', function 
         return info;
     };
 
+    // var columnDefs = [
+    //     {headerName: "转出ID", field: "fromUserId", width: 210, valueGetter: format_fromUserId},
+    //     {headerName: "转出用户", field: "fromUser", width: 100, valueGetter: format_fromUser},
+    //     {headerName: "转入ID", field: "toUserId", width: 210, valueGetter: format_toUserId},
+    //     {headerName: "转入用户", field: "toUser", width: 100, valueGetter: format_toUser},
+    //     {headerName: "交易类型", field: "type", width: 100, valueGetter: format_type},
+    //     {headerName: "交易标记", field: "flag", width: 100, valueGetter: format_flag},
+    //     {headerName: "币种", field: "ctName", width: 70, valueGetter: format_ctName},
+    //     {headerName: "操作数量", field: "amount", width: 90},
+    //     {headerName: "操作前余额", field: "balance", width: 110},
+    //     {headerName: "时间", field: "createdAt", width: 145, valueGetter: $scope.angGridFormatDateS}
+    // ];
+
     var columnDefs = [
-        {headerName: "转出ID", field: "fromUserId", width: 210, valueGetter: format_fromUserId},
-        {headerName: "转出用户", field: "fromUser", width: 100, valueGetter: format_fromUser},
-        {headerName: "转入ID", field: "toUserId", width: 210, valueGetter: format_toUserId},
-        {headerName: "转入用户", field: "toUser", width: 100, valueGetter: format_toUser},
-        {headerName: "交易类型", field: "type", width: 100, valueGetter: format_type},
-        {headerName: "交易标记", field: "flag", width: 100, valueGetter: format_flag},
-        {headerName: "币种", field: "ctName", width: 70, valueGetter: format_ctName},
-        {headerName: "操作数量", field: "amount", width: 90},
-        {headerName: "操作前余额", field: "balance", width: 110},
-        {headerName: "时间", field: "createdAt", width: 145, valueGetter: $scope.angGridFormatDateS}
+        {headerName: "发起方ID", field: "fromUserId", width: 210, valueGetter: format_fromUserId},
+        {headerName: "发起方名称", field: "fromUser", width: 100, valueGetter: format_fromUser},
+        {headerName: "发起方等级", field: "type",width: 100},        //field: "fromUserGrade"
+        {headerName: "发起方操作前金币", field: "type", width: 100},    //fromUserBalance
+        {headerName: "操作类型", field: "type", width: 100, valueGetter: format_flag},
+        {headerName: "操作金额", field: "amount", width: 100},
+        {headerName: "交易对象ID", field: "toUserId", width: 70, valueGetter: format_toUserId},
+        {headerName: "交易对象名称", field: "toUser", width: 90,valueGetter: format_toUser},
+        {headerName: "交易对象等级", field: "type", width: 110},        //toUserGrade
+        {headerName: "交易对象交易前金币", field: "type", width: 145},         //toUserBalance
+        {headerName: "操作时间", field: "createdAt", width: 145, valueGetter: $scope.angGridFormatDateS}
     ];
 
     global.agGridTranslateSync($scope, columnDefs, [
         'bank.bank.deal.header.fromUserId',
         'bank.bank.deal.header.fromUser',
+        'bank.bank.deal.header.type',
+        'bank.bank.deal.header.type',
+        'bank.bank.deal.header.type',
+        // 'bank.bank.deal.header.flag',
+        'bank.bank.deal.header.amount',
         'bank.bank.deal.header.toUserId',
         'bank.bank.deal.header.toUser',
         'bank.bank.deal.header.type',
-        'bank.bank.deal.header.flag',
-        'bank.bank.deal.header.ctName',
-        'bank.bank.deal.header.amount',
-        'bank.bank.deal.header.balance',
+        'bank.bank.deal.header.type',
         'bank.bank.deal.header.createdAt'
     ]);
 
@@ -97,6 +116,7 @@ app.controller('BankDealCtrl', ['$scope', '$state', '$http', 'global', function 
                 endDate: endDate.toString()
             },function(err,result){
                 var data = result;
+                console.log(data);
                 if (data.err) {
                     $scope.error(data.msg);
                 } else {
