0b1730dff44d1d36a8a2c4466e2cca46492f7257
Danny Wu <429305116@qq.com>
2017-04-20 15:55:27 +0800

    桌子统计修改

--- src/apps/config.js
+++ src/apps/config.js
@@ -69,7 +69,7 @@ if(domain&&host.indexOf(domain)>=0){
     document.domain = domain;
 }
 
-var omsnav = "nav_new";
+var omsnav = "nav";
 
 const pfm_cy = 'static';
 const pfm_oms = 'oms';
--- src/apps/datastatistics/js/controllers/profit.js
+++ src/apps/datastatistics/js/controllers/profit.js
@@ -1,4 +1,100 @@
 
 app.controller('ProfitCtrl', ['$scope', '$state', '$http', 'global', function ($scope, $state, $http, global) {
+    var sso = jm.sdk.sso;
+    var history = global.agentListHistory||(global.agentListHistory={});
+    $scope.pageSize = history.pageSize||$scope.defaultRows;
+    $scope.search = {};
+    $scope.search.date = $scope.search.date||{};
 
+    var page = 1;
+    var urlget = statUri+'/report/account';
+
+    $scope.dateOptions = angular.copy(global.dateRangeOptions);
+    $scope.dateOptions.opens = 'left';
+
+    $scope.tablestyle = {};
+    if($scope.isSmartDevice){
+        $scope.tablestyle = {};
+    }else{
+        $scope.tablestyle = {
+            height:$scope.app.navHeight-210+'px',
+            border:'1px solid #cccccc'
+        }
+    }
+
+    $http.get(agentUri + '/subAgents', {
+        params:{
+            token: sso.getToken(),
+            search:$scope.search.agent
+        }
+    }).success(function(result){
+        var obj = result;
+        if(obj.err){
+            $scope.error(obj.msg);
+        }else{
+            $scope.channels = obj.rows||[];
+            $scope.select = true;
+        }
+    }).error(function(msg, code){
+        $scope.errorTips(code);
+    });
+
+    $scope.left = function () {
+        if($scope.page>1){
+            --page;
+            $scope.getdata();
+        }
+    }
+    $scope.right = function () {
+        if($scope.page<$scope.pages){
+            ++page;
+            $scope.getdata();
+        }
+    };
+
+    $scope.getdata = function(_page) {
+        if(_page) page = _page;
+        $scope.moreLoading = true;
+        var search = $scope.search;
+        var date = search.date||{};
+        var startDate = date.startDate || "";
+        var endDate = date.endDate|| "";
+        var agent =search.agent;
+        $http.get(urlget, {
+            params:{
+                token: sso.getToken(),
+                page:page,
+                rows:$scope.pageSize||20,
+                isStat:true,
+                type:0,
+                startDate:startDate.toString(),
+                endDate:endDate.toString(),
+                agent:agent
+            }
+        }).success(function(result){
+            if(result.err){
+                $scope.error(result.msg);
+            }else{
+                $scope.moreLoading = false;
+                $('html,body').animate({ scrollTop: 0 }, 0);
+                $scope.playerdata = result;
+                if(result.total){
+                    $scope.nodata = false;
+                    $scope.page = result.page;
+                    $scope.pages = result.pages;
+                    $scope.total = result.total;
+                    $scope.totalnumber = global.reg(result.total);
+                }else{
+                    $scope.nodata = true;
+                }
+            }
+        }).error(function(msg, code){
+            $scope.errorTips(code);
+        });
+    }
+    $scope.getdata();
+
+    $scope.$watch('search.date', function () {
+        $scope.getdata(1);
+    });
 }]);
