d1e8e54c2077392418a8356f8d967ebd419dc77d
ayan <820695483@qq.com>
2017-04-21 16:50:10 +0800

    转账添加提示

--- src/apps/bank/js/controllers/transfer.js
+++ src/apps/bank/js/controllers/transfer.js
@@ -5,6 +5,7 @@ app.controller('BankTransferCtrl', ['$scope', '$state', '$http',  'global', '$ti
     if(transinfo){
         transinfo = JSON.parse(transinfo);
         $scope.bank.toUserId = transinfo.id;
+        $scope.transinfo = transinfo;
         $scope.uid = transinfo.uid;
         $scope.nick = transinfo.nick;
         sessionStorage.removeItem('selectedUser');
@@ -36,6 +37,7 @@ app.controller('BankTransferCtrl', ['$scope', '$state', '$http',  'global', '$ti
             }
         });
     });
+
     $scope.changeAcount = function(){
         $scope.defAccount = _.find($scope.accounts, { id: $scope.bank.fromAccountId });
     };
@@ -43,23 +45,50 @@ app.controller('BankTransferCtrl', ['$scope', '$state', '$http',  'global', '$ti
     $scope.transfer = function(){
         $scope.bank.ctCode = $scope.bank.ctCode || "jb";
         var hold = $scope.defAccount.holds[$scope.bank.ctCode||"jb"]||{amountValid:0};
-        console.log($scope.bank);
-        console.info(hold);
-        if(!$scope.isCP&&hold.amountValid<$scope.bank.amount){
-            return $scope.error('余额不足');
-        }
-        bank.transfer($scope.bank,function(err,result){
-            if (err) {
-                $timeout(function () {
-                    $scope.error(result.msg);
+
+        if(transinfo != null){
+            var account = transinfo.nick||transinfo.uid;
+            var ammount = global.reg($scope.bank.amount||0);
+            var info = global.translateByKey('search.user') + account + '<br/> '+global.translateByKey('search.transfer') + ammount;
+            if(!$scope.isCP&&hold.amountValid<$scope.bank.amount){
+                $scope.openTips({
+                    title: global.translateByKey('openTips.title'),
+                    content: global.translateByKey('openTips.balance'),
+                    cancelTitle: global.translateByKey('openTips.cancelDelContent'),
+                    singleButton: true
                 });
-            } else {
-                $timeout(function () {
-                    $scope.success('转账成功');
-                    $scope.bank={};
-                    $scope.nick='';
+            }else{
+                $scope.openTips({
+                    title:global.translateByKey('openTips.updateAccount'),
+                    content: info,
+                    okTitle:global.translateByKey('openTips.cancelDelContent'),
+                    cancelTitle:global.translateByKey('openTips.cancelTitle'),
+                    okCallback: function($s){
+                        bank.transfer($scope.bank,function(err,result){
+                            if (err) {
+                                $timeout(function () {
+                                    $scope.error(result.msg);
+                                });
+                            } else {
+                                $timeout(function () {
+                                    $scope.success('转账成功');
+                                    $scope.bank.amount = null;
+                                    $scope.bank = {} ;
+                                    $scope.transinfo = null;
+                                });
+                            }
+                        });
+                    }
                 });
             }
-        });
+        }else {
+            $scope.openTips({
+                title:global.translateByKey('openTips.title'),
+                content:global.translateByKey('openTips.selectPlayer'),
+                cancelTitle:global.translateByKey('openTips.cancelDelContent'),
+                singleButton:true
+            });
+        }
+
     };
 }]);
